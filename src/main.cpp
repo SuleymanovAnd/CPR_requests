9d3de2256d85f15f1d6a171bf8ec6070bfc774d6
Suleymanov Andrey <sasus37@gmail.com>
2022-10-30 20:41:57 +0300

    add command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    cpr::Response r;
+    std::string command;
+    while (command != "exit"){
+        std::cout << ": ";
+        std::cin >> command;
+
+        if (command == "get"){
+            r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+            std::cout << r.text << std::endl;
+        }
+       if (command == "post"){
+           r = cpr::Post(cpr::Url("http://httpbin.org/post"));
+           std::cout << r.text << std::endl;
+       }
+       if (command == "put"){
+           r = cpr::Put(cpr::Url("http://httpbin.org/put"));
+           std::cout << r.text << std::endl;
+       }
+        if (command == "delete"){
+            r = cpr::Delete(cpr::Url("http://httpbin.org/delete"));
+            std::cout << r.text << std::endl;
+        }
+        if (command == "patch"){
+            r = cpr::Patch(cpr::Url("http://httpbin.org/patch"));
+            std::cout << r.text << std::endl;
+        }
+    }
+
+
 }
